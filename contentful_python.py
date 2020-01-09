71e6dd7c31dc7f56e9bc089867f77d93b540e0d6
Alec Johnson <alecijohnson@gmail.com>
2020-01-09 16:02:42 +0000

    Added 'get field' function and details for the version info field

--- contentful_python.py
+++ contentful_python.py
@@ -1,4 +1,5 @@
 import contentful_management
+import re
 
 with open(r'C:\Users\aleci\Python\KEYS\contentful.txt', 'r') as file:
     KEY = file.readline()
@@ -41,7 +42,7 @@ tools = ['tool',
 metadata = ['topic']
 
 #%%
-def add_field(env, content_type, field, appearance):
+def add_field(env, content_type, field, appearance, replace = 0):
     """Add a new field to a given content type.
     Pass the field as a dictionary including at least id and name.
     Appearance should be a separate dict for the 'controls'
@@ -54,9 +55,51 @@ def add_field(env, content_type, field, appearance):
     for f in interface.controls:
         if f['fieldId'] == field['id']:
             f['settings'] = appearance
+            break
     interface.save()
     return 0
 
+def get_existing(env, content_type, field_id):
+    """ Gets details of an existing field so I can set up an add field
+    Test this against a list comprehension version"""
+    t = env.content_types().find(content_type)
+    id_converted = re.sub(r'(?=[A-Z])', '_', field_id).lower()
+    for f in t.fields:
+        if f.id == id_converted:
+            result = f
+            break
+    interface = t.editor_interfaces().find()
+    for f in interface.controls:
+        if f['fieldId'] == field_id:
+            appearance = f
+            break
+    return {'Field': result.to_json(), 'Appearance': appearance}
+
+#%%
+current = get_existing(ENVIRONMENT, 'adviceCollection', 'startTaskList')
+print(current)
+
+#%%
+# Version info
+version_info = {'name': 'Version information', 
+                'id': 'versionInformation', 
+                'type': 'Text', 
+                'localized': False, 
+                'omitted': False, 
+                'required': False, 
+                'disabled': False, 
+                'validations': []}
+appearance = {'fieldId': 'versionInformation', 
+            'settings': {'helpText': "What's changed in this version of the content?"}, 
+            'widgetId': 'markdown', 
+            'widgetNamespace': 'builtin'}
+
+add_field(ENVIRONMENT, 'adviceCollectionAdviser', version_info, appearance)
+add_field(ENVIRONMENT, 'adviceList', version_info, appearance)
+
+
+#%%
+# Task lists
 
 task_list = {'id': 'startTaskList',
              'name': 'Start of task list?',
@@ -66,7 +109,5 @@ task_list = {'id': 'startTaskList',
 settings = {'trueLabel': 'Yes',
             'falseLabel': 'No'}
 add_field(ENVIRONMENT, 'adviceCollection', task_list, settings)
-
-#%%
 add_field(ENVIRONMENT, 'adviceCollectionAdviser', task_list, settings)
 add_field(ENVIRONMENT, 'adviceList', task_list, settings)
